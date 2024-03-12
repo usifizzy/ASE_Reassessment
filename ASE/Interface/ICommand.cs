c405e26f2ffbb8f70b571401dcc43c4b5b7f4115
usifizzy <Usifizzy@yahoo.com>
2024-03-12 09:02:04 +0100

    interfaces

---
+++ ASE/Interface/ICommand.cs
@@ -0,0 +1,10 @@
+﻿
+using ASE_Programming_Lang;
+
+namespace ASE_Programming_Lang.Interface
+{
+    public interface ICommand
+    {
+        void Execute(Canvas canvas, string[] arguments);
+    }
+}
---
+++ ASE/Interface/IGraphicsCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_Programming_Lang.Interface
+{
+    /// <summary>
+    /// Represents an interface for graphics commands.
+    /// </summary>
+    public interface IGraphicsCommand
+    {
+        /// <summary>
+        /// Executes the graphics command.
+        /// </summary>
+        /// <param name="shape">The Graphics object to perform drawing operations.</param>
+        /// <param name="arguments">An array of arguments passed to the command.</param>
+        /// <param name="canvas">The Canvas object representing the drawing surface.</param>
+        void Execute(Graphics shape, string[] arguments, Canvas canvas);
+    }
+}
