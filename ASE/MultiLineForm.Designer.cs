55f1f797012ac3d19a56fbc91becbf0510d85b71
usifizzy <Usifizzy@yahoo.com>
2024-03-12 08:17:52 +0100

    form design

--- ASE/MultiLineForm.Designer.cs
+++ ASE/MultiLineForm.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.RunScriptBtn = new System.Windows.Forms.Button();
+            this.LoadScriptBtn = new System.Windows.Forms.Button();
+            this.SaveScriptBtn = new System.Windows.Forms.Button();
+            this.ResetScript = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(309, 406);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // RunScriptBtn
+            // 
+            this.RunScriptBtn.Location = new System.Drawing.Point(12, 437);
+            this.RunScriptBtn.Name = "RunScriptBtn";
+            this.RunScriptBtn.Size = new System.Drawing.Size(109, 37);
+            this.RunScriptBtn.TabIndex = 1;
+            this.RunScriptBtn.Text = "Run";
+            this.RunScriptBtn.UseVisualStyleBackColor = true;
+            this.RunScriptBtn.Click += new System.EventHandler(this.RunScriptBtn_Click);
+            // 
+            // LoadScriptBtn
+            // 
+            this.LoadScriptBtn.Location = new System.Drawing.Point(212, 437);
+            this.LoadScriptBtn.Name = "LoadScriptBtn";
+            this.LoadScriptBtn.Size = new System.Drawing.Size(109, 37);
+            this.LoadScriptBtn.TabIndex = 2;
+            this.LoadScriptBtn.Text = "Load Script";
+            this.LoadScriptBtn.UseVisualStyleBackColor = true;
+            // 
+            // SaveScriptBtn
+            // 
+            this.SaveScriptBtn.Location = new System.Drawing.Point(12, 495);
+            this.SaveScriptBtn.Name = "SaveScriptBtn";
+            this.SaveScriptBtn.Size = new System.Drawing.Size(109, 35);
+            this.SaveScriptBtn.TabIndex = 3;
+            this.SaveScriptBtn.Text = "Save Script";
+            this.SaveScriptBtn.UseVisualStyleBackColor = true;
+
+            // 
+            // ResetScript
+            // 
+            this.ResetScript.Location = new System.Drawing.Point(211, 495);
+            this.ResetScript.Name = "ResetScript";
+            this.ResetScript.Size = new System.Drawing.Size(109, 35);
+            this.ResetScript.TabIndex = 4;
+            this.ResetScript.Text = "Reset";
+            this.ResetScript.UseVisualStyleBackColor = true;
+            this.ResetScript.Click += new System.EventHandler(this.ResetScript_Click);
+            // 
+            // MultiLineForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(332, 556);
+            this.Controls.Add(this.ResetScript);
+            this.Controls.Add(this.SaveScriptBtn);
+            this.Controls.Add(this.LoadScriptBtn);
+            this.Controls.Add(this.RunScriptBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "MultiLineForm";
             this.Text = "MultiLineForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button RunScriptBtn;
+        private System.Windows.Forms.Button LoadScriptBtn;
+        private System.Windows.Forms.Button SaveScriptBtn;
+        private System.Windows.Forms.Button ResetScript;
     }
 }
